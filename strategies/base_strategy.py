c6e8e8e005c9107d7df8d00048e5bb4a4d737caa
Andrew Kuchev <0coming.soon@gmail.com>
2014-09-16 20:21:57 +0600

    Added useful methods to BaseStrategy

--- strategies/base_strategy.py
+++ strategies/base_strategy.py
@@ -2,11 +2,20 @@
 This module provides class for representing base strategy.
 Every strategy should inherit this class.
 """
+from model.HockeyistState import HockeyistState
+from model.HockeyistType import HockeyistType
+from model.Puck import Puck
+from model.Player import Player
 from model.ActionType import ActionType
 from model.Game import Game
 from model.Hockeyist import Hockeyist
 from model.Move import Move
+from model.Unit import Unit
 from model.World import World
+from utils.iter import first
+from utils.point import Point
+
+from math import pi
 
 
 __all__ = ['BaseStrategy']
@@ -24,6 +33,181 @@ class BaseStrategy:
         self.world = world
         self.game = game
         self.move = move
+
+    #region Utils
+
+    @property
+    def distance_to_puck(self) -> float:
+        return self.me.get_distance_to_unit(self.world.puck)
+
+    @property
+    def our_team_own_puck(self) -> bool:
+        owner = self.puck_owner
+        return owner is not None and owner.teammate
+
+    @property
+    def own_puck(self):
+        return self.our_team_own_puck and self.puck_owner.id == self.me.id
+
+    @property
+    def opponent_team_own_puck(self):
+        return not self.our_team_own_puck and self.puck_owner is not None
+
+    @property
+    def puck_owner(self) -> Hockeyist:
+        return self.get_hockeyist_by_id(self.world.puck.owner_hockeyist_id)
+
+    def get_hockeyist_by_id(self, hockeyist_id) -> Hockeyist:
+        return first(self.world.hockeyists, lambda h: h.id == hockeyist_id)
+
+    @property
+    def goal_net_center(self) -> Point:
+        return self.get_goal_net_center(self.player)
+
+    @property
+    def opponent_goal_net_center(self) -> Point:
+        return self.get_goal_net_center(self.opponent)
+
+    @property
+    def player(self) -> Player:
+        return first(self.world.players, lambda p: p.me)
+
+    @property
+    def opponent(self) -> Player:
+        return first(self.world.players, lambda p: not p.me)
+
+    @staticmethod
+    def get_goal_net_center(player: Player) -> Point:
+        return Point(player.net_front, (player.net_bottom + player.net_top) / 2)
+
+    @property
+    def puck(self) -> Puck:
+        return self.world.puck
+
+    @property
+    def stick_length(self):
+        return self.game.stick_length
+
+    @property
+    def stick_sector(self):
+        return self.game.stick_sector
+
+    def get_distance_to_unit(self, unit):
+        return self.me.get_distance_to_unit(unit)
+
+    def get_distance(self, x, y):
+        return self.get_angle_to_unit(Point(x, y))
+
+    def get_angle_to_unit(self, unit):
+        return self.me.get_angle_to_unit(unit)
+
+    @property
+    def angle(self):
+        return self.me.angle
+
+    @property
+    def nearest_teammate(self):
+        # The first item is the player itself, so, return the second
+        return sorted(self.my_hockeyists, key=self.get_distance_to_unit)[1]
+
+    @property
+    def angle_to_nearest_teammate(self):
+        return self.get_angle_to_unit(self.nearest_teammate)
+
+    def can_pass_to(self, teammate):
+        return self.get_angle_to_unit(teammate) < self.pass_sector / 2
+
+    def get_pass_angle_to(self, hockeyist):
+        absolute_pass_angle = self.get_angle_to_unit(hockeyist)
+        relative_pass_angle = absolute_pass_angle - self.pass_angle
+
+        while relative_pass_angle > pi:
+            relative_pass_angle -= 2.0 * pi
+
+        while relative_pass_angle < -pi:
+            relative_pass_angle += 2.0 * pi
+
+        return relative_pass_angle
+
+    @property
+    def pass_angle(self):
+        return self.move.pass_angle
+
+    @pass_angle.setter
+    def pass_angle(self, angle):
+        assert isinstance(angle, (int, float))
+        self.move.pass_angle = float(angle)
+
+    @property
+    def pass_sector(self):
+        return self.game.pass_sector
+
+    def get_real_distance_to_unit(self, other: Unit):
+        return self.get_distance_to_unit(other) - self.me.radius - other.radius
+
+    def can_influence_by_stick(self, other: Unit) -> bool:
+        return (self.get_distance_to_unit(other) < self.stick_length and
+                abs(self.get_angle_to_unit(other)) < self.stick_sector / 2)
+
+    @property
+    def can_influence_puck(self) -> bool:
+        return self.can_influence_by_stick(self.puck)
+
+    @property
+    def opponent_hockeyists(self) -> [Hockeyist]:
+        return [h for h in self.hockeyists if not h.teammate]
+
+    @property
+    def my_hockeyists(self) -> [Hockeyist]:
+        return [h for h in self.hockeyists if h.teammate and h.type != HockeyistType.GOALIE]
+
+    @property
+    def last_action(self):
+        return self.me.last_action
+
+    @property
+    def max_effective_swing_ticks(self):
+        return self.game.max_effective_swing_ticks
+        
+    @property
+    def swing_ticks(self):
+        return self.me.swing_ticks
+
+    @property
+    def hockeyists(self) -> [Hockeyist]:
+        return self.world.hockeyists
+
+    @property
+    def can_influence_opponent(self):
+        return any(map(self.can_influence_by_stick, self.opponent_hockeyists))
+
+    @property
+    def state(self) -> HockeyistState:
+        return self.me.state
+
+    @property
+    def angle_to_puck(self):
+        return self.get_angle_to_unit(self.puck)
+
+    @property
+    def goal_net_top_corner(self):
+        return self.get_goal_net_top_corner(self.player)
+
+    @property
+    def goal_net_top_corner(self):
+        return self.get_goal_net_bottom_corner(self.player)
+
+    @staticmethod
+    def get_goal_net_top_corner(player):
+        return Point(player.net_front, player.net_top)
+
+    @staticmethod
+    def get_goal_net_bottom_corner(player):
+        return Point(player.net_front, player.net_bottom)
+
+    #endregion
+
+    #region Properties
 
     @property
     def me(self):
@@ -93,3 +277,5 @@ class BaseStrategy:
         :return: By default returns ActionType.NONE
         """
         return ActionType.NONE
+
+    #endregion
