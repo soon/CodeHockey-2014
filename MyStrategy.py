e769acd1a306f811e52b9e9abeee929c270ea6de
Andrew Kuchev <0coming.soon@gmail.com>
2014-09-17 18:43:19 +0600

    ForwardStrategy and NoGoalieStrategy are included into MyStrategy The score after 100 games: 139-147 (My-Local)

--- MyStrategy.py
+++ MyStrategy.py
@@ -1,18 +1,29 @@
 from model.Game import Game
+from model.HockeyistType import HockeyistType
 from model.Move import Move
 from model.Hockeyist import Hockeyist
 from model.World import World
+from strategies.forward_strategy import ForwardStrategy
 from strategies.base_strategy import BaseStrategy
 from strategies.defence_strategy import DefenceStrategy
-from strategies.dumb_strategy import DumbStrategy
+from strategies.no_goalies_strategy import NoGoaliesStrategy
 
 
 class MyStrategy:
 
     @staticmethod
+    def no_goalies(world):
+        return not any(h.type == HockeyistType.GOALIE for h in world.hockeyists)
+
+    @staticmethod
     def create_strategy(me, world, game, move) -> BaseStrategy:
-        return [DumbStrategy,
-                DefenceStrategy][me.teammate_index](me, world, game, move)
+        if MyStrategy.no_goalies(world):
+            strategy = NoGoaliesStrategy
+        else:
+            strategy = [ForwardStrategy,
+                        DefenceStrategy][me.teammate_index]
+
+        return strategy(me, world, game, move)
 
     def move(self, me: Hockeyist, world: World, game: Game, move: Move):
         strategy = self.create_strategy(me, world, game, move)
