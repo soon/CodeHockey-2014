e21c47241f143ba20b3b5c6458808c82eb442709
Andrew Kuchev <0coming.soon@gmail.com>
2014-09-17 18:38:11 +0600

    Implemented strategy for overtime

---
+++ strategies/no_goalies_strategy.py
@@ -0,0 +1,31 @@
+"""
+This module provides classes for representing strategy in case if there is no goalies
+"""
+from strategies.base_strategy import BaseStrategy
+from strategies.defence_strategy import DefenceStrategy
+from strategies.simple_striker_strategy import SimpleStrikerStrategy
+
+
+__all__ = ['NoGoaliesStrategy']
+
+
+class NoGoaliesStrategy(BaseStrategy):
+
+    def __init__(self, me, world, game, move):
+        super().__init__(me, world, game, move)
+
+        strategy = SimpleStrikerStrategy if self.our_team_own_puck else DefenceStrategy
+
+        self.strategy = strategy(me, world, game, move)
+
+    @property
+    def speed_up(self):
+        return self.strategy.speed_up
+
+    @property
+    def turn(self):
+        return self.strategy.turn
+
+    @property
+    def action(self):
+        return self.strategy.action
