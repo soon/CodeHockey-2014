d26f0ca487b43226a09db3c1ff8378afb74b9b06
Andrew Kuchev <0coming.soon@gmail.com>
2014-09-23 00:15:33 +0600

    Simple striker strategy rewritten using states

--- simple_striker_strategy.py
+++ simple_striker_strategy.py
@@ -1,9 +1,16 @@
 """
 This module provides classes for representing simply striker strategy
 """
+from enum import Enum
 from model.ActionType import ActionType
 from base_strategy import BaseStrategy
 
+
+class StrategyState(Enum):
+    kick_all_opponents = 1
+    take_puck = 2
+    turn_to_opponent_goal_net = 3
+    attack = 4
 
 class SimpleStrikerStrategy(BaseStrategy):
 
@@ -13,12 +20,62 @@ class SimpleStrikerStrategy(BaseStrategy):
         self._allowed_angle = 0.07
 
     @property
+    def speed_up(self):
+        return {
+            StrategyState.kick_all_opponents: 1.0,
+            StrategyState.take_puck: 1.0,
+            StrategyState.turn_to_opponent_goal_net: 0.0,
+            StrategyState.attack: 0.0
+        }[self.state]
+
+    @property
     def turn(self):
-        return self.get_angle_to_unit(self.opponent_goal_net_center)
+        return {
+            StrategyState.kick_all_opponents: self.angle_to_nearest_opponent,
+            StrategyState.take_puck: self.angle_to_puck,
+            StrategyState.turn_to_opponent_goal_net: self.angle_to_opponent_goal_net_center,
+            StrategyState.attack: self.angle_to_opponent_goal_net_center
+        }[self.state]
 
     @property
     def action(self):
-        if self.get_angle_to_unit(self.opponent_goal_net_center) < self._allowed_angle:
-            return ActionType.STRIKE
+        return {
+            StrategyState.kick_all_opponents: self.take_puck_or_prevent_attack_or_attack_opponent,
+            StrategyState.take_puck: self.take_puck_or_prevent_attack_or_attack_opponent,
+            StrategyState.turn_to_opponent_goal_net: ActionType.NONE,
+            StrategyState.attack: ActionType.STRIKE
+        }[self.state]
+
+    @staticmethod
+    def initial_info():
+        return {
+            'state': StrategyState.kick_all_opponents
+        }
+
+    @property
+    def state(self):
+        return self.info['state']
+
+    @state.setter
+    def state(self, value):
+        self.info['state'] = value
+
+    def update_state(self):
+        new_state = self.get_next_state(self.state)
+
+        while self.state != new_state:
+            self.state = new_state
+            new_state = self.get_next_state(self.state)
+
+    def get_next_state(self, state):
+        if not self.our_team_own_puck:
+            state = StrategyState.take_puck
         else:
-            return ActionType.NONE
+            state = StrategyState.kick_all_opponents
+
+        if state == StrategyState.take_puck and self.own_puck:
+           state = StrategyState.turn_to_opponent_goal_net
+
+        elif (state == StrategyState.turn_to_opponent_goal_net and
+                      self.get_angle_to_unit(self.opponent_goal_net_center) < self._allowed_angle):
+            state = StrategyState.attack
