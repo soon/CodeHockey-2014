a634e9052b38929fe2c868c0d3d9521e82c46ad5
Andrew Kuchev <0coming.soon@gmail.com>
2015-11-02 18:55:04 +0500

    Added `future_opponent_position` method

--- kicker_strategy.py
+++ kicker_strategy.py
@@ -12,11 +12,11 @@ class KickerStrategy(BaseStrategy):
 
     @property
     def turn(self):
-        return self.get_angle_to_unit(self.opponent_to_be_attacked)
+        return self.get_angle_to_unit(self.future_opponent_position)
 
     @property
     def action(self):
-        if self.can_influence_by_stick(self.opponent_to_be_attacked):
+        if self.can_influence_by_stick(self.future_opponent_position):
             return self.kick_opponent_action
         else:
             return ActionType.CANCEL_STRIKE
@@ -24,3 +24,7 @@ class KickerStrategy(BaseStrategy):
     @property
     def opponent_to_be_attacked(self):
         return self.nearest_opponent
+
+    @property
+    def future_opponent_position(self):
+        return self.optimal_position_to_interact_with(self.opponent_to_be_attacked)
